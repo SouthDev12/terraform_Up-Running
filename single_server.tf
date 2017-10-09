5ab6c41f64023df2e9514385ea56fef239a3b43b
Jay Southern <southdev12@gmail.com>
2017-10-09 14:25:59 -0600

    Add ASG and save single server

--- single_server.tf
+++ single_server.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_launch_configuration" "example" {
+resource "aws_instance" "example" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
-  security_groups = ["${aws_security_group.instance.id}"]
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -13,8 +13,8 @@ resource "aws_launch_configuration" "example" {
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
-  lifecycle {
-    create_before_destroy = true
+  tags {
+    Name = "terraform-example"
   }
 }
 
@@ -26,24 +26,6 @@ resource "aws_security_group" "instance" {
     to_port   = "${var.server_port}"
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = "${aws_launch_configuration.example.id}"
-  availability_zones = ["${data.aws_availability_zones.all.names}"]
-
-  min_size = 2
-  max_size = 10
-
-  tag {
-    key   = "Name"
-    value = "terraform-asg-example"
-    propogate_at_launch = true
   }
 }
 
